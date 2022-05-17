226ca21b987400bad5b5b36f0e5c5bf3df1c979a
Antoniellin <89428807+Antoniellin@users.noreply.github.com>
2022-05-16 21:32:07 -0300

    Update data_hora.java

--- data_hora.java
+++ data_hora.java
@@ -2,17 +2,31 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Calendar;
+import java.time.Duration;
+import java.time.Instant;
+
 
 public class A002 {
   public static void main (String[] args) {
     
-    DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd-");
     Calendar calendario = Calendar.getInstance();
-    System.out.println("Data:" + df.format(calendario.getTime()));
+    //System.out.println("Data:" + df.format(calendario.getTime()));
 
     Date dataHoraAtual = new Date();
-    String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
+    String hora = new SimpleDateFormat("HH-mm-ss a").format(dataHoraAtual);
+    System.out.println(df.format(calendario.getTime()) + hora);
+    
 
-    System.out.println("Hora: "+ hora);
+    Calendar dataCadastro = Calendar.getInstance();
+    dataCadastro.set(2022, 1, 1);
+    Calendar hoje = Calendar.getInstance();
+
+    //calcula diferença
+    int meses = (hoje.get(Calendar.YEAR) * 12 + hoje.get(Calendar.MONTH))
+        - (dataCadastro.get(Calendar.YEAR) * 12 + dataCadastro.get(Calendar.MONTH));
+
+        System.out.println("Diferença de meses: "+ meses);
+
   }
 }
